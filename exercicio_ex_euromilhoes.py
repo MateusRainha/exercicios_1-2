acf3d2efc7132ff0da2ce75704383ce54ab9255f
MateusRainha <mateusferreirarainha@enta.pt>
2022-09-29 12:10:36 +0000

    exercicio2d.py

---
+++ exercicio_ex_euromilhoes.py
@@ -0,0 +1,17 @@
+import random
+
+def get_random(ini, fim):
+    return random.randrange(ini, fim + 1)
+
+if __name__ == '__main__':
+    nums = [0, 0, 0, 0, 0]
+    for x in range(len(nums)):
+
+        while True:
+            onumero = get_random(1, 50)
+            if onumero not in nums:
+                nums[x] = onumero
+                break
+
+    print('Números')
+    print(nums)
