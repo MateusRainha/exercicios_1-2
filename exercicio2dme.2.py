e6e54c3ade4d0fc9e85cd69187df9184d193b5d3
MateusRainha <mateusferreirarainha@enta.pt>
2022-09-29 10:15:32 +0000

    exercicio2d.py

--- exercicio2dme.2.py
+++ exercicio2dme.2.py
@@ -7,18 +7,29 @@ vendas =[
 
 if __name__ == '__main__':
     # Obter input
-    for x in range(2):
-        for y in range(5):
+    for x in range(len(vendas)):
+        for y in range(len(vendas[x])):
             while True:
                 try:
                     vendas[x][y] = int(input(f'Insira vendas de {tipos[x]} para a ilha {ilhas[y]} '))
                     break
                 except ValueError:
                     print(f'Insira um valor válido para vendas de {tipos[x]} na ilha {ilhas[y]}')
-    print(vendas)
+    for venda in vendas:
+        print(venda)
 
     # Imprimir vendas por tipo
-
-
+    for x in range(len(vendas)):
+        total = 0
+        for y in range(len(vendas[x])):
+            total += vendas[x][y]
+        print(f'Total de vendas de {tipos[x]} = {total}')
 
     # Imprimir vendas por ilha
+    z = 0
+    for y in range(len(vendas[z])):
+        z += 1
+        total = 0
+        for x in range(len(vendas)):
+            total += vendas[x][y]
+        print(f'Total de vendas para {ilhas[y]} = {total}')
