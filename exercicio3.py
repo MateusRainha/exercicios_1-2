3530c357c6057f16abd191f601629c8a06720659
MateusRainha <mateusferreirarainha@enta.pt>
2022-09-29 11:54:05 +0000

    exercicio2d.py

---
+++ exercicio3.py
@@ -0,0 +1,24 @@
+import random
+
+def get_random(ini, fim):
+    """
+    Esta função devolve um número aleatório entre ini e fim inclusive
+    :param ini: início do intervalo
+    :param fim: fim do intervalo
+    :return: número aleatório
+    """
+    return random.randrange(ini, fim + 1)
+
+if __name__ == '__main__':
+    nums = [0, 0, 0, 0, 0]
+    for x in range(len(nums)):
+
+        while True:
+            onumero = get_random(1, 5)
+            if onumero not in nums:
+                nums[x] = onumero
+                break
+
+    print(nums)
+
+    # Máquina que produz números únicos num determinado intervalo
