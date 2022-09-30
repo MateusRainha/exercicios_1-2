802531a2660597a4284fab5c4beddd5175f070f9
MateusRainha <mateusferreirarainha@enta.pt>
2022-09-30 10:34:58 +0000

    exercicio2d.py

---
+++ dicionario.py
@@ -0,0 +1,21 @@
+pessoa =(1, 'Maria', {'morada': 'Rua de Cima, 1', 'cp': 9500}, 12.50)
+
+meses = {'Janeiro', 'Fevereiro', 'Março'}
+
+if __name__ == '__main__':
+    """
+    print(pessoa[2].keys())
+    print(pessoa[2].values())
+    print()
+    for k in pessoa[2].keys():
+        print(k)
+    print()
+    for v in pessoa[2].values():
+        print(v)
+    print()
+    for k in pessoa[2].keys():
+        print(f'{k} = {pessoa[2][k]}')
+    """
+
+meses.add('Abril')
+print(meses)
